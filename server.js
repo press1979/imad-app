76d4f2c2811467ae35027202d8087ee4c966026f
press1979 <press1979@gmail.com>
2018-02-21 10:57:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ res.sendFile('This is articl one''));
 });
 
 app.get('/article-two', function (req, res) {
